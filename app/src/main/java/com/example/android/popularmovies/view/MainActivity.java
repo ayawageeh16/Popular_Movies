6243d70d29cc10c7e0c7b6523fee0d56007d594f
ayawageeh16 <ayawageeh16@gmail.com>
2018-03-12 15:45:22 +0200

    phase one completed

--- app/src/main/java/com/example/android/popularmovies/view/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/view/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.android.popularmovies.view;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -11,6 +16,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.popularmovies.adapter.MovieAdapter;
 import com.example.android.popularmovies.data.TrailerModel;
@@ -25,11 +31,14 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     final static String ACTIVITY_STATE_KEY = "Activity state";
     TextView errorMessageTv;
     RecyclerView recyclerView;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
+    NavigationView navigationView ;
     String sortBy ;
     String errorMessageDisplay;
     List<MovieModel> movieDetails = new ArrayList<MovieModel>();
@@ -45,23 +54,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         errorMessageTv =findViewById(R.id.Show_error_message);
+        drawerLayout = findViewById(R.id.drawer_layout);
         recyclerView = findViewById(R.id.rv_movies);
+        navigationView = findViewById(R.id.navigation_view);
         setRecyclerView(recyclerView);
+        toggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        setDrawerLayout(drawerLayout,toggle);
+        navigationView.setNavigationItemSelectedListener(this);
+
 
         if (savedInstanceState != null) {
             if (savedInstanceState.containsKey(ACTIVITY_STATE_KEY)) {
                 sortBy = savedInstanceState.getString(ACTIVITY_STATE_KEY);
                 try {searchQueryExecute();
                 } catch (MalformedURLException e) {
-                    e.printStackTrace();}
-            }
-        } else {
+                    e.printStackTrace();}}} else {
             sortBy ="popular";
-            try {searchQueryExecute();
-            } catch (MalformedURLException e) {
-                e.printStackTrace();}
-        }
+            try {searchQueryExecute();} catch (MalformedURLException e) {
+                e.printStackTrace();}}
     }
+
+    private void setDrawerLayout (DrawerLayout drawerLayout , ActionBarDrawerToggle toggle) {
+        toggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+
     private void  setRecyclerView (RecyclerView recyclerView){
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this,2);
         recyclerView.setLayoutManager(gridLayoutManager);
@@ -87,6 +108,30 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setVisibility(View.INVISIBLE);
         errorMessageTv.setText(errorMessageDisplay);
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemClicked = item.getItemId();
+        if (itemClicked == R.id.popular_movies_sort) {
+            sortBy = "popular";
+            try {
+                searchQueryExecute();
+                drawerLayout.closeDrawers();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        }
+        else if (itemClicked ==R.id.top_rated_sort) {
+            sortBy = "top_rated";
+            try {
+                searchQueryExecute();
+                drawerLayout.closeDrawers();
+            } catch (MalformedURLException e) {
+            }
+        }
+        return false;
+    }
+
     // execute network tasks and JSON in background
     public class FetchMovieData implements AsyncTaskCompleteListener{
         @Override
@@ -111,28 +156,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu,menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int itemClicked = item.getItemId();
-        if (itemClicked == R.id.sort_byPopular) {
-            sortBy = "popular";
-            try {
-                searchQueryExecute();
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            }
-        }
-        else if (itemClicked ==R.id.sort_byTopRated) {
-            sortBy ="top_rated";
-            try {
-                searchQueryExecute();
-            } catch (MalformedURLException e) {
-            }
+
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
