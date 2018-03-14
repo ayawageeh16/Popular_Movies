05ba3e97299283db4b188bc2b0ef55c9ed9a3e4e
ayawageeh16 <ayawageeh16@gmail.com>
2018-03-14 03:59:12 +0200

    SqlLite database is added and insertion is implemented

--- app/src/main/java/com/example/android/popularmovies/adapter/FavouritesAdapter.java
+++ app/src/main/java/com/example/android/popularmovies/adapter/FavouritesAdapter.java
@@ -1,9 +1,77 @@
 package com.example.android.popularmovies.adapter;
 
+import android.database.Cursor;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.android.popularmovies.R;
+import com.example.android.popularmovies.data.FavouritesContract;
+import com.squareup.picasso.Picasso;
+
 /**
  * Created by Lenovo on 13/03/2018.
  */
 
-public class FavouritesAdapter {
+public class FavouritesAdapter extends RecyclerView.Adapter<FavouritesAdapter.FavouritesViewHolder> {
 
+    Cursor cursor;
+    int position;
+
+    public FavouritesAdapter(Cursor cursor){
+        this.cursor =cursor;
+    }
+
+    @Override
+    public FavouritesViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View row = LayoutInflater.from(parent.getContext()).inflate(R.layout.favourite_row,parent,false);
+        FavouritesViewHolder holder = new FavouritesViewHolder(row);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(FavouritesViewHolder holder, int position) {
+       if (!cursor.moveToPosition(position)) {
+           return;
+       }else {
+           holder.Bind(cursor);
+           this.position=position;
+           holder.itemView.setTag(cursor.getLong(cursor.getColumnIndex(FavouritesContract.FavouritesEntry.COLUMN_MOVIE_ID)));
+       }
+    }
+
+    public void swapCursor (Cursor newCursor){
+       if(cursor!=null)cursor.close();
+           cursor=newCursor;
+       if (newCursor!=null){
+           this.notifyDataSetChanged();
+           this.notifyItemChanged(this.position);
+       }
+    }
+
+    @Override
+    public int getItemCount() {
+        return cursor.getCount();
+    }
+
+    public class FavouritesViewHolder extends RecyclerView.ViewHolder{
+        ImageView poster;
+        TextView title;
+        public FavouritesViewHolder(View itemView) {
+            super(itemView);
+            poster=itemView.findViewById(R.id.favourite_poster);
+            title=itemView.findViewById(R.id.favourite_title);
+        }
+        public void Bind(final Cursor cursor){
+            String s =String.valueOf(cursor.getString(cursor.getColumnIndex(FavouritesContract.FavouritesEntry.COLUMN_MOVIE_POSTER)));
+            Picasso.with(poster.getContext())
+                    .load(s)
+                    .into(poster);
+            title.setText(String.valueOf(cursor.getString(cursor.getColumnIndex(FavouritesContract.FavouritesEntry.COLUMN_MOVIE_TITLE))));
+        }
+    }
 }
--- app/src/main/java/com/example/android/popularmovies/view/Favourites.java
+++ app/src/main/java/com/example/android/popularmovies/view/Favourites.java
@@ -1,15 +1,69 @@
 package com.example.android.popularmovies.view;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 
 import com.example.android.popularmovies.R;
+import com.example.android.popularmovies.adapter.FavouritesAdapter;
+import com.example.android.popularmovies.data.FavouritesContract;
+import com.example.android.popularmovies.data.FavouritesDBHelper;
+
+import static android.support.v7.widget.LinearLayoutManager.VERTICAL;
 
 public class Favourites extends AppCompatActivity {
 
+    RecyclerView recyclerView ;
+    SQLiteDatabase mDB;
+    FavouritesAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favourites);
+        recyclerView = findViewById(R.id.favourites_rv);
+        setRecyclerView(recyclerView);
+        FavouritesDBHelper dbHelper = new FavouritesDBHelper(this);
+        mDB =dbHelper.getReadableDatabase();
+        Cursor cursor = getAllFavourites();
+        adapter=new FavouritesAdapter(cursor);
+        recyclerView.setAdapter(adapter);
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.LEFT|ItemTouchHelper.RIGHT) {
+
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+               long movieId = (long) viewHolder.itemView.getTag();
+               deleteFavourite(movieId);
+               adapter.swapCursor(getAllFavourites());
+            }
+        }).attachToRecyclerView(recyclerView);
+
+
     }
+    private void setRecyclerView (RecyclerView recyclerView){
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this,VERTICAL,true);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+    }
+    private Cursor getAllFavourites () {
+        return mDB.query(FavouritesContract.FavouritesEntry.TABLE_NAME,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null);
+    }
+    private boolean deleteFavourite(long movieId){
+       return mDB.delete(FavouritesContract.FavouritesEntry.TABLE_NAME,
+                FavouritesContract.FavouritesEntry.COLUMN_MOVIE_ID+ "=" +movieId,null) >0;
+    }
+
 }
